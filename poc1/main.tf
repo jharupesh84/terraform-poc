cf48181dbdee01fda770e6f3c23cbaff0836d06f
jharupesh84 <jharupesh84@gmail.com>
2025-03-12 22:15:00 +0530

    commented s3 code

--- poc1/main.tf
+++ poc1/main.tf
@@ -9,6 +9,6 @@ resource "aws_instance" "rupesh" {
   ami = "ami-053b0d53c279acc90" # change this
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "poc1rupeshbabli123rup"
-}
+# resource "aws_s3_bucket" "s3_bucket" {
+#   bucket = "poc1rupeshbabliGyanu"
+# }
